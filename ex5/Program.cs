7e3faf2b7751422fb034d15d299e2b2ca86bb276
Aleksandr <makasar83@mail.ru>
2022-08-30 08:04:47 +0300

    add ex5 name

---
+++ ex5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, МАША, привет!!");
+}
+else
+{
+    Console.WriteLine("Привет: ");
+    Console.WriteLine(username);
+}
